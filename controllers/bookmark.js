84cf7ce31d749e2165a76f11263ec99c324d9bf9
geethaKrishnaCH <chgeethakrishna@gmail.com>
2024-03-31 16:55:04 +0530

    bookmark apis

--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -1,10 +1,21 @@
-import { insertBookmark } from "../db/bookmark.js";
+import { deleteBookmark, insertBookmark } from "../db/bookmark.js";
 
 export const addBookmark = async (req, res, next) => {
   try {
     const { id: userId } = req.user;
     const { recipeId } = req.body;
     await insertBookmark(userId, recipeId);
+    return res.json({ success: true });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const removeBookmark = async (req, res, next) => {
+  try {
+    const { id: userId } = req.user;
+    const { recipeId } = req.body;
+    await deleteBookmark(userId, recipeId);
     return res.json({ success: true });
   } catch (err) {
     next(err);
--- routes/bookmark.js
+++ routes/bookmark.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { addBookmark, getAllBookmarks } from "../controllers/bookmark.js";
+import {
+  addBookmark,
+  getAllBookmarks,
+  removeBookmark,
+} from "../controllers/bookmark.js";
 import { validateAccessToken } from "../util/middleware.js";
 
 const router = express.Router();
 
 router.get("/", validateAccessToken, getAllBookmarks);
+router.post("/remove", validateAccessToken, removeBookmark);
 router.post("/add", validateAccessToken, addBookmark);
 
 export default router;
